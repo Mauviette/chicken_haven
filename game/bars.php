56e5d59fb53df5cca6fb37d896da579da86c7eb2
Mauviette <julesbouquet4@gmail.com>
2024-12-01 20:25:58 +0100

    Page de profil

--- game/bars.php
+++ game/bars.php
@@ -3,7 +3,7 @@
 echo '<!-- Barre de navigation -->
     <div class="navbar">
     <div class="profile-section">
-        <a href="settings.php" class="profile-link">
+        <a href="my_profile.php" class="profile-link">
         <img src="images/player_icon.png" alt="Profil" class="profile-icon">
         <span class="username">' . $_SESSION['displayname'] . '</span>
         </a>
@@ -13,6 +13,8 @@ echo '<!-- Barre de navigation -->
     <!-- Barre latérale toujours visible -->
     <div class="sidebar">
     <ul>
+        <li><a href="main.php">Accueil</a></li>
+        <li><a href="hatchery.php">Couvoir</a></li>
         <li><a href="shop.php">Boutique</a></li>
         <li><a href="leaderboard.php">Classement</a></li>
     </ul>
